26a2b61eecc1891e7ea00f1fa144f9fa4bed8362
Desmond <adabedesmond@gmail.com>
2024-08-09 07:41:34 -0700

    added payment controller

---
+++ payment/controllers/payment_controller.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const makePayment = {
+    receivePayment: async (req, res) => {
+        const { email, amount, callback_url, reference } = req.body;
+
+        const fData = {
+            'amount': amount,
+            'email': email,
+        }
+
+        console.log(fData);
+
+        try {
+            const response = await axios.post(
+                'https://api.paystack.co/transaction/initialize',
+                {
+                    amount: amount * 100,
+                    email : email, // Amount in kobo
+                    callback_url,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            const { status, data } = response.data;
+             console.log(data);
+             
+            if (status) {
+                return res.json({
+                    success: true,
+                    authorization_url: data.authorization_url,
+                    reference : data.reference,
+                    data
+                });
+            } else {
+                return res.status(400).json({
+                    success: false,
+                    message: 'Transaction initialization failed'
+                });
+            }
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                message: 'Server error',
+                error: error.message
+            });
+        }
+    }
+}
+
+module.exports = makePayment;
+
