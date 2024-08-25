897e1ef26a419eaa5dec1972374f978a44f23efd
Desmond <adabedesmond@gmail.com>
2024-08-25 15:47:20 -0700

    modified

--- admin/routes/admin_controller.js
+++ admin/routes/admin_controller.js
@@ -3,9 +3,13 @@ const adminCredentialsRouter = express.Router();
 const verify = require('../credentials/verify_credentials');
 const update = require('../credentials/update_credentials');
 const addCredentials = require('../credentials/add_credentials');
+const changePassword = require('../credentials/change_password')
+
 
 adminCredentialsRouter.post('/login', verify.verifyCredentials);
 adminCredentialsRouter.post('/update-credentials', update.updateCredentials);
 adminCredentialsRouter.post('/add-credentials',addCredentials.addCredentials );
+adminCredentialsRouter.post('/change-password', changePassword.changePassword);
+
 
 module.exports = adminCredentialsRouter;
--- admin/routes/get_transactions_router.js
+++ admin/routes/get_transactions_router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const getTransactionsRouter = express.Router();
 const getTransactions = require('../transactions/controller/get_all_transactions');
+const search = require('../transactions/controller/search_transaction');
 
 getTransactionsRouter.get('/get-transactions', getTransactions.getTransactions);
+getTransactionsRouter.get('/search-transactions', search.searchTransaction);
 
 module.exports = getTransactionsRouter;
