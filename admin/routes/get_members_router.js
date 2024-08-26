000c856ddf2b0d4beb60cf1e7dce584fa9cd61bf
Desmond <adabedesmond@gmail.com>
2024-08-25 18:27:15 -0700

    modified

--- admin/routes/get_members_router.js
+++ admin/routes/get_members_router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const getMembersRouter = express.Router();
 const getMembers = require('../getMembers/get_members');
+const search =require('../getMembers/search_members');
 
 getMembersRouter.get('/get-members', getMembers.getAllMembers);
+getMembersRouter.get('/search-members', search.searchMembers);
 
 module.exports = getMembersRouter;
