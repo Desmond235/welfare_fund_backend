841555f0c81b11f1ef15244a4aba9aab2c02ab4a
Desmond <adabedesmond@gmail.com>
2024-08-26 02:40:47 -0700

    modified

--- admin/routes/get_members_router.js
+++ admin/routes/get_members_router.js
@@ -2,8 +2,12 @@ const express = require('express');
 const getMembersRouter = express.Router();
 const getMembers = require('../getMembers/get_members');
 const search =require('../getMembers/search_members');
+const totalMembers = require('../getMembers/total_members');
+const countGender = require('../getMembers/get_gender');
 
 getMembersRouter.get('/get-members', getMembers.getAllMembers);
 getMembersRouter.get('/search-members', search.searchMembers);
+getMembersRouter.get('/total-members', totalMembers);
+getMembersRouter.get('/count-gender', countGender);
 
 module.exports = getMembersRouter;
--- admin/routes/get_transactions_router.js
+++ admin/routes/get_transactions_router.js
@@ -2,8 +2,10 @@ const express = require('express');
 const getTransactionsRouter = express.Router();
 const getTransactions = require('../transactions/controller/get_all_transactions');
 const search = require('../transactions/controller/search_transaction');
+const totalAmount = require('../transactions/controller/total_amount');
 
 getTransactionsRouter.get('/get-transactions', getTransactions.getTransactions);
 getTransactionsRouter.get('/search-transactions', search.searchTransaction);
+getTransactionsRouter.get('/total-amount', totalAmount)
 
 module.exports = getTransactionsRouter;
