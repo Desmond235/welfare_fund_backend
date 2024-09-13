6332d23eddd76dbc9cf4fb5dcfc3df87f9ad42a3
Desmond <adabedesmond@gmail.com>
2024-09-12 19:08:38 -0700

    added get transactions

---
+++ payment/controllers/get_transactions.js
@@ -0,0 +1,33 @@
+const db = require('../../dbConnection');
+
+const transactions = {
+    getTransactions: async (req, res) => {
+       try {
+        const {id } = req.params
+         const query = 'SELECT * FROM transaction WHERE id = ?'
+         db.query(
+            query,
+            [id],
+            (err, result) => {
+                if(err){
+                    return res.status(400).json({
+                        message: "An error occurred"
+                    });
+                }else{
+                   return res.status(200).json({
+                        message: "Transactions fetched successfully",
+                        data: result
+                    })
+                }
+            }
+         )
+        
+       } catch (error) {
+          return res.status(500).json({
+            message: error.message || "Server Error"
+         })
+       }
+    }
+}
+
+module.exports = transactions;
