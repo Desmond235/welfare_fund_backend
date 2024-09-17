ec68c7c249fd90dcdd47335ef32c44ee76078462
Desmond <adabedesmond@gmail.com>
2024-09-16 22:29:03 -0700

    modified

--- admin/transactions/controller/total_amount.js
+++ admin/transactions/controller/total_amount.js
@@ -2,16 +2,18 @@ const db = require('../../../dbConnection')
 
 const totalAmount = async (req, res) => {
     const query = `
-        SELECT SUM(amount) AS total_amount
-         FROM transaction;
+        SELECT DATE_FORMAT(date, '%Y-%m') AS month, SUM(amount) AS total_amount
+         FROM transaction 
+         GROUP BY month
+         ORDER BY month;
         `;
-    db.query(query, (err, result) => {
+    db.query(query, (err, results) => {
         if (err) {
             return res.status(400).json({
                 message: 'An error occurred while fetching total amount from the database'
             });
         }
-        return res.json(result[0]);
+        return res.json(results);
     })
 }
 
