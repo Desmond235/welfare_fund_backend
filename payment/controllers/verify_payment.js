d836145512efe75b7f7604da6299aa736ade6078
Desmond <adabedesmond@gmail.com>
2024-10-15 21:30:38 -0700

    modified

--- payment/controllers/verify_payment.js
+++ payment/controllers/verify_payment.js
@@ -34,7 +34,7 @@ const verifyPayment = {
 
                             return res.json({
                                 id: result.insertId,
-                                userId: parseInt(id),
+                                userId: parseInt(id),   
                                 success: true,
                                 message: 'Payment verified successfully',
                                 data:{
